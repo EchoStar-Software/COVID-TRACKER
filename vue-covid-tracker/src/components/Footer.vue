6cf34c5107d3af020fbbe10e9aaee3a76e5d104d
ksherm337 <22063898+KSherman97@users.noreply.github.com>
2022-09-15 22:16:10 -0400

    updated the build

---
+++ vue-covid-tracker/src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer class="footer mt-auto bg-dark">
+    <div class="container">
+        <center>
+            EchoStar Software
+        <br /> <p>Designed with <a href="https://getbootstrap.com/">BootStrap</a> | Built on <a href="https://kit.svelte.dev/">Svelte Framework</a></p>
+        </center>
+    </div>
+    </footer>
+</template>
